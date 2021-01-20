8a62557c491541a24fc9d4aaed7628c99afae162
Manel Puig <manel.puig@ub.edu>
2021-01-20 17:28:45 +0100

    wed5

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,9 @@
+# CMake generated Testfile for 
+# Source directory: /home/mpuig/rubot_mecanum_ws/src
+# Build directory: /home/mpuig/rubot_mecanum_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("nexus_4wd_mecanum_gazebo")
+subdirs("nexus_4wd_mecanum_description")
