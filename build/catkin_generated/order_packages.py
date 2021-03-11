e4b1bcc99cc6115e6ab6de6fe7d676c5d146c77d
Manel Puig <manel.puig@ub.edu>
2021-03-11 16:36:17 +0100

    Delete Build & Devel folders

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = '/home/mpuig/rubot_mecanum_ws/src'
-whitelisted_packages = ''.split(';') if '' != '' else []
-blacklisted_packages = ''.split(';') if '' != '' else []
-underlay_workspaces = '/home/mpuig/rubot_mecanum_ws/devel;/opt/ros/melodic'.split(';') if '/home/mpuig/rubot_mecanum_ws/devel;/opt/ros/melodic' != '' else []
--- build/mecanum_control/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/mpuig/rubot_mecanum_ws/src/mecanum_control
-# Build directory: /home/mpuig/rubot_mecanum_ws/build/mecanum_control
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
