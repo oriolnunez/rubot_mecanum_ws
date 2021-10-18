a71a23eef6172c3c7a468c42c23f252f477d296f
manelpuig <manel.puig@ub.edu>
2021-10-18 10:04:14 +0200

    added go2pose

--- src/robot_control/nexus_control/src/rubot_go2pose.py
+++ src/robot_control/nexus_control/src/rubot_go2pose.py
@@ -3,7 +3,7 @@ import rospy
 from geometry_msgs.msg import Twist, Pose, Quaternion
 from nav_msgs.msg import Odometry
 from math import pow, atan2, sqrt
-from tf.transformations import quaternion_from_euler
+from tf.transformations import euler_from_quaternion, quaternion_from_euler
 
 class nexus:
 
@@ -31,72 +31,70 @@ class nexus:
         """Callback function which is called when a new message of type Pose is
         received by the subscriber."""
         self.odom = data
-        self.x_pose = round(self.odom.pose.position.x, 2)
-        self.y_pose = round(self.odom.pose.position.y, 2)
-        self.z_pose = round(self.odom.pose.position.z, 2)
-        self.qx_orientation = round(self.odom.pose.orientation.x, 2)
-        self.qy_orientation = round(self.odom.pose.orientation.y, 2)
-        self.qz_orientation = round(self.odom.pose.orientation.z, 2)
-        self.qw_orientation = round(self.odom.pose.orientation.w, 2)
-        print(self.odom)
-        print(self.x_pose)
-        print(self.qw_orientation)
-
+        self.x_pose = round(self.odom.pose.pose.position.x, 2)
+        self.y_pose = round(self.odom.pose.pose.position.y, 2)
+        self.z_pose = round(self.odom.pose.pose.position.z, 2)
+        self.orientation_q = self.odom.pose.pose.orientation
+        self.orientation_list = [self.orientation_q.x, self.orientation_q.y, self.orientation_q.z, self.orientation_q.w]
+        self.rpw = euler_from_quaternion (self.orientation_list)
+        self.yaw = self.rpw[2]
+        
     def euclidean_distance(self, goal_odom):
         """Euclidean distance between current pose and the goal."""
-        return sqrt(pow((self.x_goal - self.x_pose), 2) +
-                    pow((self.y_goal - self.y_pose), 2))
+        return sqrt(pow((goal_odom.pose.pose.position.x - self.x_pose), 2) +
+                    pow((goal_odom.pose.pose.position.y - self.y_pose), 2))
 
-    def linear_vel(self, goal_pose, constant=1):
+    def linear_vel(self, goal_odom, constant=1):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * self.euclidean_distance(goal_pose)
+        return constant * self.euclidean_distance(goal_odom)
 
-    def steering_angle(self, goal_pose):
+    def steering_angle(self, goal_odom):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return atan2(goal_pose.y - self.pose.y, goal_pose.x - self.pose.x)
+        return atan2(goal_odom.pose.pose.position.y - self.y_pose, goal_odom.pose.pose.position.x - self.x_pose)
 
-    def angular_vel(self, goal_pose, constant=6):
+    def angular_vel(self, goal_odom, constant=6):
         """See video: https://www.youtube.com/watch?v=Qh15Nol5htM."""
-        return constant * (self.steering_angle(goal_pose) - self.pose.theta)
+        return constant * (self.steering_angle(goal_odom) - self.yaw)
 
     def move2pose(self):
         """Moves the turtle to the goal."""
         goal_odom = Odometry()
 
         # Get the input from the user.
-        goal_odom.odom.pose.position.x = self.x_goal
-        goal_odom.odom.pose.position.y = self.y_goal
-        goal_odom.odom.pose.orientation.x = self.q_goal[1]
-        goal_odom.odom.pose.orientation.y = self.q_goal[2]
-        goal_odom.odom.pose.orientation.z= self.q_goal[3]
-        goal_odom.odom.pose.orientation.w= self.q_goal[4]
+        goal_odom.pose.pose.position.x = self.x_goal
+        goal_odom.pose.pose.position.y = self.y_goal
+        goal_odom.pose.pose.orientation.x = self.q_goal[0]
+        goal_odom.pose.pose.orientation.y = self.q_goal[1]
+        goal_odom.pose.pose.orientation.z= self.q_goal[2]
+        goal_odom.pose.pose.orientation.w= self.q_goal[3]
 
-        print(goal_odom)
+        rospy.loginfo("goal : " + str(goal_odom))
+        
         # Please, insert a number slightly greater than 0 (e.g. 0.01).
         distance_tolerance = 0.01
 
         vel_msg = Twist()
 
-        """ while self.euclidean_distance(goal_pose) >= distance_tolerance:
+        while self.euclidean_distance(goal_odom) >= distance_tolerance:
 
             # Porportional controller.
             # https://en.wikipedia.org/wiki/Proportional_control
 
             # Linear velocity in the x-axis.
-            vel_msg.linear.x = self.linear_vel(goal_pose)
+            vel_msg.linear.x = self.linear_vel(goal_odom)
             vel_msg.linear.y = 0
             vel_msg.linear.z = 0
 
             # Angular velocity in the z-axis.
             vel_msg.angular.x = 0
             vel_msg.angular.y = 0
-            vel_msg.angular.z = self.angular_vel(goal_pose)
+            vel_msg.angular.z = self.angular_vel(goal_odom)
 
             # Publishing our vel_msg
             self.velocity_publisher.publish(vel_msg)
 
             # Publish at the desired rate.
-            self.rate.sleep() """
+            self.rate.sleep()
 
         # Stopping our robot after the movement is over.
         vel_msg.linear.x = 0
