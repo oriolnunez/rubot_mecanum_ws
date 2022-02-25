96f6ba7faae9e9f160c1391bafbcf77161d3dc55
‘manelpuig’ <manel.puig@ub.edu>
2022-02-25 14:08:48 +0100

    Deleted nexus

--- Documentation/files/rbpi4_src/rubot_ws/Noetic/rubot_rbpi4_ws/Doc/Arduino_files/ArduinoROS_rubot_mecanum/ArduinoROS_rubot_mecanum.ino
+++ Documentation/files/rbpi4_src/rubot_ws/Noetic/rubot_rbpi4_ws/Doc/Arduino_files/ArduinoROS_rubot_mecanum/ArduinoROS_rubot_mecanum.ino
@@ -1,135 +1,3 @@
-# **rUBot Mecanum driver**
-
-Create a folder Arduino with all the installed libraries and code.
-
-To test the functionality, we could create a first code to test de Odometry publishing:
-- define a circle trajectory
-- publish the tf frames of moving frame using tf.transform_broadcaster
-- publish the Odometry messages in /odom topic
-
-Create a first code program in arduino:
-```python
-// Rosserial pubsub exemple
-#include <ros.h>
-#include <ros/time.h>
-#include <tf/tf.h>
-#include <tf/transform_broadcaster.h>
-#include <nav_msgs/Odometry.h>
-#include <sensor_msgs/Range.h>
-#include <geometry_msgs/Twist.h>
-
-ros::NodeHandle  nh;
-double x = 1.0;
-double y = 0.0;
-double theta = 1.57;
-double vx = 0.0;
-double w = 0.0;
-char base_link[] = "/base_link";
-char odom_link[] = "/odom";
-
-void messageCb( const geometry_msgs::Twist& cmd_msg) {
-  digitalWrite(13, HIGH-digitalRead(13));   // blink the led
-  nh.loginfo("cmd_vel received!");
-  vx=cmd_msg.linear.x;
-  w=cmd_msg.angular.z;
-  nh.loginfo("vel published!:");
-  //nh.loginfo(vx);
-  
-}
-
-ros::Subscriber<geometry_msgs::Twist> sub_vel("cmd_vel", messageCb );
-
-sensor_msgs::Range range_msg;
-ros::Publisher pub_range( "range_data", &range_msg);
-nav_msgs::Odometry odom;
-ros::Publisher odom_pub("odom", &odom);
-
-geometry_msgs::TransformStamped t;
-tf::TransformBroadcaster broadcaster;
-
-
-void setup()
-{
-  pinMode(13, OUTPUT);
-  nh.initNode();
-  broadcaster.init(nh);
-  nh.advertise(odom_pub);
-  nh.advertise(pub_range);
-  nh.subscribe(sub_vel);
-}
-
-void loop()
-{
-  // drive in a circle
-  double dx = 0.2;
-  double dtheta = 0.18;
-  x += cos(theta)*dx*0.1;
-  y += sin(theta)*dx*0.1;
-  theta += dtheta*0.1;
-  if(theta > 3.14)
-    theta=-3.14;
-    
-  // tf odom->base_link
-  t.header.frame_id = odom_link;
-  t.child_frame_id = base_link;
-  
-  t.transform.translation.x = x;
-  t.transform.translation.y = y;
-  
-  t.transform.rotation = tf::createQuaternionFromYaw(theta);
-  t.header.stamp = nh.now();
-  
-  broadcaster.sendTransform(t);
-  
-  // Odom Publishing
-  odom.header.stamp = nh.now();;
-  odom.header.frame_id = "odom";
-  odom.pose.pose.position.x = x;
-  odom.pose.pose.position.y = y;
-  odom.pose.pose.position.z = 0.0;
-  odom.pose.pose.orientation =tf::createQuaternionFromYaw(theta);;
-
-  odom.child_frame_id = "base_link";
-  odom.twist.twist.linear.x = 3;
-  odom.twist.twist.linear.y = 4;
-  odom.twist.twist.angular.z = 5;
-  //odom_pub.publish(&odom);
-
-  // Range Publishing
-  range_msg.range = 3;
-  //pub_range.publish(&range_msg);
-  
-  nh.spinOnce();
-  delay(50);
-}
-```
-Open a terminal and type:
-```shell
-roscore
-rosrun rosserial_python serial_node.py /dev/ttyACM0
-```
-You can see the Odom in rviz
-![](./Images/2b_odom_TF.png)
-
-## **2. Closed loop code**
-
-This final code contains:
-- Subscriber to /cmd_vel topic 
-- Publisher to /odom topic
-- Publisher to /imu topic
-
->Take care about:
->- Motor connections
-![](./Images/2b_motor.jpg)
->- Shield schematics
-![](./Images/2b_shield.jpg)
->- Pin number of encoders and PWM
-![](./Images/2b_pinout.jpg)
->- When you want to test the program in ROS, you need to close the arduino IDE to make available the USB connection
-
-The final code will be:
-
-```python
 
 #include <ros.h>
 #include <ros/time.h>
@@ -149,8 +17,7 @@ The final code will be:
 //#define HDW_DEBUG  //aquesta opcio es per provar que envia correctament pel port serie les dades de la IMU i dels encodes, despres s'ha de desactivar  amb //
 
 #if !defined(HDW_DEBUG)
-ros::NodeHandle nh;
-//ros::NodeHandle_<ArduinoHardware, 5, 5, 512, 1024> nh;
+ros::NodeHandle_<ArduinoHardware, 5, 5, 512, 1024> nh;
 tf::TransformBroadcaster broadcaster;
 
 
@@ -246,7 +113,7 @@ void setup()
 //  Imu.SetupDevice();
   #if !defined(HDW_DEBUG)
   nh.initNode();
-  broadcaster.init(nh);
+  //broadcaster.init(nh);
   nh.subscribe(sub);
   nh.subscribe(resub);
   nh.advertise(odom_pub);
@@ -313,7 +180,7 @@ Serial.println(gz);
   odom.twist.twist.linear.y = vyi;
   odom.twist.twist.angular.z = omegai;
 
-  //odom_pub.publish(&odom);
+  odom_pub.publish(&odom);
 
 
   if((millis()-lastctrl)>1000*ctrlrate){
@@ -323,8 +190,3 @@ Serial.println(gz);
   #endif
  
 }
-```
-To publish a cmd_vel, type:
-```shell
-rostopic pub -r 1 /cmd_vel geometry_msgs/Twist '[2, 0, 0]' '[0, 0, 2]'
-```
